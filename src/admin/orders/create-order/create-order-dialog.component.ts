eeeb5ca0ebd277b6677d6a343df027269085bf6a
Sebastian <sebastian.ruzanowski@gmail.com>
2020-10-02 14:18:53 +0200

    Adding administration control

--- src/admin/orders/create-order/create-order-dialog.component.ts
+++ src/admin/orders/create-order/create-order-dialog.component.ts
@@ -8,47 +8,45 @@ import {
 import { finalize } from 'rxjs/operators'
 import { BsModalRef } from 'ngx-bootstrap/modal'
 import {
-    CreateOrderDto,
-    OrderServiceProxy,
-} from '../../../shared/service-proxies/service-proxies';
-import {AppComponentBase} from '../../../shared/app-component-base';
+  CreateOrderDto,
+  OrderServiceProxy
+} from '../../../shared/service-proxies/service-proxies'
+import { AppComponentBase } from '../../../shared/app-component-base'
 
 @Component({
   templateUrl: 'create-order-dialog.component.html'
 })
 export class CreateOrderDialogComponent extends AppComponentBase
-  implements OnInit
-{
-    saving = false
-    order: CreateOrderDto = new CreateOrderDto()
+  implements OnInit {
+  saving = false
+  order: CreateOrderDto = new CreateOrderDto()
 
-    @Output() onSave = new EventEmitter<any>()
+  @Output() onSave = new EventEmitter<any>()
 
-    constructor(
-        injector: Injector,
-        public _orderService: OrderServiceProxy,
-        public bsModalRef: BsModalRef
-    ) {
-        super(injector)
-    }
+  constructor(
+    injector: Injector,
+    public _orderService: OrderServiceProxy,
+    public bsModalRef: BsModalRef
+  ) {
+    super(injector)
+  }
 
-    ngOnInit(): void {
-    }
+  ngOnInit(): void {}
 
-    save(): void {
-        this.saving = true
+  save(): void {
+    this.saving = true
 
-        this._orderService
-            .create(this.order)
-            .pipe(
-                finalize(() => {
-                    this.saving = false
-                })
-            )
-            .subscribe(() => {
-                this.notify.info(this.l('SavedSuccessfully'))
-                this.bsModalRef.hide()
-                this.onSave.emit()
-            })
-    }
+    this._orderService
+      .create(this.order)
+      .pipe(
+        finalize(() => {
+          this.saving = false
+        })
+      )
+      .subscribe(() => {
+        this.notify.info(this.l('SavedSuccessfully'))
+        this.bsModalRef.hide()
+        this.onSave.emit()
+      })
+  }
 }
--- src/admin/orders/edit-order/edit-order-dialog.component.ts
+++ src/admin/orders/edit-order/edit-order-dialog.component.ts
@@ -7,48 +7,51 @@ import {
 } from '@angular/core'
 import { finalize } from 'rxjs/operators'
 import { BsModalRef } from 'ngx-bootstrap/modal'
-import { OrderDto, OrderServiceProxy } from '@shared/service-proxies/service-proxies';
-import {AppComponentBase} from '../../../shared/app-component-base';
+import {
+  OrderDto,
+  OrderServiceProxy
+} from '@shared/service-proxies/service-proxies'
+import { AppComponentBase } from '../../../shared/app-component-base'
 
 @Component({
   templateUrl: 'edit-order-dialog.component.html'
 })
 export class EditOrderDialogComponent extends AppComponentBase
   implements OnInit {
-    saving = false
-    tenant: OrderDto = new OrderDto()
-    id: number
+  saving = false
+  tenant: OrderDto = new OrderDto()
+  id: number
 
-    @Output() onSave = new EventEmitter<any>()
+  @Output() onSave = new EventEmitter<any>()
 
-    constructor(
-        injector: Injector,
-        public _tenantService: OrderServiceProxy,
-        public bsModalRef: BsModalRef
-    ) {
-        super(injector)
-    }
+  constructor(
+    injector: Injector,
+    public _tenantService: OrderServiceProxy,
+    public bsModalRef: BsModalRef
+  ) {
+    super(injector)
+  }
 
-    ngOnInit(): void {
-        this._tenantService.get(this.id).subscribe((result: OrderDto) => {
-            this.tenant = result
+  ngOnInit(): void {
+    this._tenantService.get(this.id).subscribe((result: OrderDto) => {
+      this.tenant = result
+    })
+  }
+
+  save(): void {
+    this.saving = true
+
+    this._tenantService
+      .update(this.tenant)
+      .pipe(
+        finalize(() => {
+          this.saving = false
         })
-    }
-
-    save(): void {
-        this.saving = true
-
-        this._tenantService
-            .update(this.tenant)
-            .pipe(
-                finalize(() => {
-                    this.saving = false
-                })
-            )
-            .subscribe(() => {
-                this.notify.info(this.l('SavedSuccessfully'))
-                this.bsModalRef.hide()
-                this.onSave.emit()
-            })
-    }
+      )
+      .subscribe(() => {
+        this.notify.info(this.l('SavedSuccessfully'))
+        this.bsModalRef.hide()
+        this.onSave.emit()
+      })
+  }
 }
