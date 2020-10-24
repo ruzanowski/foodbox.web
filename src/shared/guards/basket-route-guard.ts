a1350d21010f5901af4841004e23ffba5de27a28
Sebastian <sebastian.ruzanowski@gmail.com>
2020-10-24 23:57:32 +0200

    Basket guard and pipe missing added

--- src/shared/guards/basket-route-guard.ts
+++ src/shared/guards/basket-route-guard.ts
@@ -8,13 +8,13 @@ export class BasketRouteGuard implements CanActivate {
   constructor(private _router: Router, private _basketService: OrderService) {}
 
   canActivate(route: ActivatedRouteSnapshot): boolean {
-    // if (!this._basketService.anyItems()) {
-    //   this._router.navigate(['/' + 'items'])
-    //   abp.notify.warn(
-    //     'Nie możesz przejść do tej strony, nie masz nic w koszyku.'
-    //   )
-    //   return false
-    // }
+    if (!this._basketService.anyItems()) {
+      this._router.navigate(['/' + 'items'])
+      abp.notify.warn(
+        'Nie możesz przejść do tej strony, nie masz nic w koszyku.'
+      )
+      return false
+    }
     return true
   }
 }
